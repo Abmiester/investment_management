6a4c925ed808f5666b858a65379bd1e27cb36dfe
Abraar Ahmed <abraar10ahmed@gmail.com>
2020-05-14 23:59:58 -0400

    cir model, cash vs zc bond

--- edhec_risk_kit.py
+++ edhec_risk_kit.py
@@ -559,3 +559,41 @@ def funding_ratio(assets, liabilities, r):
     Computes funding ratio of some assets given liabilities and interest rate
     '''
     return assets/pv(liabilities, r)
+
+
+def cir(n_years=10, n_scenarios=1, a=0.05, b=0.03, sigma=0.05, steps_per_year=12, r_0=None):
+    '''
+    implements the Cox Ingersoll Ross model
+    '''
+    if r_0 is None:
+        r_0 = b
+    r_0 = ann_to_int(r_0)
+    dt = 1/steps_per_year
+    
+    num_steps = int(n_years*steps_per_year)+1
+    shock = np.random.normal(0, scale=np.sqrt(dt), size=(num_steps, n_scenarios))
+    rates = np.empty_like(shock)
+    rates[0] = r_0
+    
+    # for price generation
+    h = np.sqrt(a**2 + 2*sigma**2)
+    prices = np.empty_like(shock)
+    
+    def price(ttm, r):
+        _A = ((2*h*np.exp((h+a)*ttm/2))/(2*h+(h+a)*(np.exp(h*ttm)-1)))**(2*a*b/sigma**2)
+        _B = (2*(np.exp(h*ttm)-1))/(2*h + (h+a)*(np.exp(h*ttm)-1))
+        _P = _A*np.exp(-_B*r)
+        return _P
+    prices[0] = price(n_years, r_0)
+    
+    for step in range(1, num_steps):
+        r_t = rates[step-1]
+        d_r_t = a*(b-r_t)*dt + sigma*np.sqrt(r_t)*shock[step]
+        rates[step] = abs(r_t + d_r_t)
+        # generate prices at step as well
+        prices[step] = price(n_years-step*dt, rates[step])
+    
+    rates = pd.DataFrame(data=inst_to_ann(rates), index=range(num_steps))
+    prices = pd.DataFrame(data=prices, index=range(num_steps))
+        
+    return rates, prices
