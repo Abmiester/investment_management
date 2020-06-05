a4dab118325da8dca0729f7488250a857a5967ea
Abraar Ahmed <abraar10ahmed@gmail.com>
2020-06-05 13:41:33 -0400

    black-litterman method

--- edhec_risk_kit.py
+++ edhec_risk_kit.py
@@ -1001,3 +1001,98 @@ def weight_gmv(r, cov_estimator=sample_cov, **kwargs):
     '''
     est_cov = cov_estimator(r, **kwargs)
     return gmv(est_cov)
+
+
+def as_colvec(x):
+    '''
+    Given an array of vectors,
+    Returns an nd array with axis changed to columns
+    '''
+    if (x.ndim == 2):
+        return x
+    else:
+        return np.expand_dims(x, axis=1)
+    
+    
+def implied_returns(delta, sigma, w):
+    '''
+    Obtain the implied expected returns by reverse engineering the weights
+    Inputs:
+        delta: Risk Aversion Coefficient (scalar)
+        sigma: Variance-Covariance Matrix (N x N) as DataFrame
+        w: Portfolio weights (N x 1) as Series
+    Returns an N x 1 vector of returns as Series
+    '''
+    ir = delta * sigma.dot(w).squeeze() # squeeze() turns one column dataframe to series
+    ir.name = 'Implied Returns'
+    return ir
+
+
+def proportional_prior(sigma, tau, p):
+    '''
+    Returns the He-Litterman simplified Omega
+    Inputs:
+        sigma: N x N Covariance Matrix as DataFrame
+        tau: a scalar
+        p: a K x N DataFrame linking Q and Assets
+    Returns a P x P DataFrame, a matrix representing prior uncertainties 
+    '''
+    # can use .dot() or @ for matrix multiplication
+    helit_omega = p.dot(tau * sigma).dot(p.T)
+    return pd.DataFrame(np.diag(np.diag(helit_omega.values)), index=p.index, columns=p.index)
+
+
+def bl(w_prior, sigma_prior, p, q, omega=None, delta=2.5, tau=0.02):
+    '''
+    Computes the posterior expected returns based on the original black litterman reference model
+    Inputs:
+        w_prior must be an N x 1 vector of weights, a series
+        sigma_prior is an N x N covariance matrix, a DataFrame
+        p must be a K x N matrix linking Q and the Assets, a DataFrame
+        q must be K x 1 vector of views, a Series
+        omega must be a K x K matrix, a DataFrame or None
+        if omega is None, we assume it is proportional to variance of the prior
+        delta and tau are scalars
+    '''
+    if omega is None:
+        omega = proportional_prior(sigma_prior, tau, p)
+    # number of assets
+    N = w_prior.shape[0]
+    # number of views
+    K = q.shape[0]
+    # reverse engineer weights to get pi
+    pi = implied_returns(delta, sigma_prior, w_prior)
+    # scale sigma by uncertainty scaling factor
+    sigma_prior_scaled = tau * sigma_prior
+    # posterior estimate of mean by master formula
+    mu_bl = pi + sigma_prior_scaled.dot(p.T).dot(inv(p.dot(sigma_prior_scaled).dot(p.T) + omega).dot(q - p.dot(pi).values))
+    # posterior estimate of uncertainty of mu_bl
+    sigma_bl = sigma_prior + sigma_prior_scaled - sigma_prior_scaled.dot(p.T).dot(inv(p.dot(sigma_prior_scaled).dot(p.T) + omega)).dot(p).dot(sigma_prior_scaled)
+    return mu_bl, sigma_bl
+
+
+def inverse(d):
+    '''
+    Invert the dataframe by inverting the underlying matrix
+    '''
+    return pd.DataFrame(inv(d.values), index=d.columns, columns=d.index)
+
+
+def w_msr(sigma, mu, scale=True):
+    '''
+    No constrains on this max-sharpe ratio (unlike the previous one we wrote, which is constrained)
+    Optimal (Tangent/Max Sharpe Ratio) Portfolio weights by using the Markowitz Optimization Procedure
+        mu is the vector of excess expected returns
+        sigma must be an N x N matrix as a DataFrame and mu a column vector as a series
+    '''
+    w = inverse(sigma).dot(mu)
+    if scale:
+        w = w/sum(w) # assumes all w is positive
+    return w
+
+
+def w_star(delta, sigma, mu):
+    '''
+    Weights in the optimal portfolio, w*
+    '''
+    return (inverse(sigma).dot(mu))/delta
